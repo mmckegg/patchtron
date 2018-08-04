fbf693223ede30c443737afd2e517e93bf2884c5
Matt McKegg <matt@wetsand.co.nz>
2018-08-04 15:42:30 +1200

    fix "View full thread" count on profiles

--- views/feed-item.js
+++ views/feed-item.js
@@ -4,7 +4,7 @@ const { h } = require('mutant')
 module.exports = function renderItem (msg, {connection, i18n}) {
   return h('FeedEvent', [
     renderMessage(msg, {connection, i18n}),
-    msg.totalReplies > 3 ? h('a.full', {href: msg.key}, ['View full thread' + ' (', msg.totalReplies, ')']) : null,
+    msg.totalReplies > msg.latestReplies.length ? h('a.full', {href: msg.key}, ['View full thread' + ' (', msg.totalReplies, ')']) : null,
     h('div.replies', [
       msg.latestReplies.map(msg => renderMessage(msg, {connection, i18n}))
     ])
